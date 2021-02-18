371931cd6fcaf7cfddc90fa398820a5c5ec82ca2
NEOMorpheus33 <jof6453@g.harvard.edu>
2021-02-18 00:24:28 -0500

    Create index-view.php

---
+++ practice/index-view.php
@@ -0,0 +1,26 @@
+<doctype html>
+<html lang='en'>
+
+<head>  
+
+<title>e15 project 1 </title>
+<meta charset='utf=8'>
+<link href=data:, rel=icon>
+
+</head>
+
+<body>
+
+    <h1>e15 Project 1 <h1>
+    String <?php echo $inputString ?>
+    Is big word?  
+
+    <php if (isBigWord) {?>
+    Yes
+    <?php } else { ?>
+    No
+    <?php { ?> 
+
+</body>
+
+</html>
