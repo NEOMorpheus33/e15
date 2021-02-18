a3579e783f366ed0e75dc3a54dad178499aaa8da
NEOMorpheus33 <jof6453@g.harvard.edu>
2021-02-18 00:15:45 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>   
+
+<html lang='en'>
+
+<body>        
+
+<?php
+
+function is Palindrome(inputString)
+
+{ 
+
+    return strlen($inputString)  > 7;
+
+    //if strlen($inputString) > 7) {
+        // return true; 
+
+   // } else {
+       //return false;
+    // }
+} 
+
+inputString ='cat'; 
+
+$isBigWord = isBigWord($inputString); //t or f 
+
+
+require 'index-view.php';
+
+?> 
+
+</body>
+
+</html>
