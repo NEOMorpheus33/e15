4c299247ed355371da5fe7d32b64115e78facc1c
NEOMorpheus33 <jof6453@g.harvard.edu>
2021-02-18 01:30:22 -0500

    index-view.php

---
+++ p1/index-view.php
@@ -0,0 +1,24 @@
+<!doctype html> 
+<html lang= 'en'> 
+
+<head>
+
+<title>e15 p1</title>
+<meta charset='utf-8'>
+<link href=data:, rel=icon>
+
+</head>
+
+<body>
+
+<h1>e15 P1</h1> 
+String: <?php echo $inputString ?>
+Is big word?
+<?php if ($isBigWord) { ?>
+    Yes
+    <?php }else { ?>
+    No
+    <?php {?> 
+
+</body>
+</html> 
