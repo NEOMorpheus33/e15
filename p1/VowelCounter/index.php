4d15534b47497234ff2f8511a54a5e8c7395f39a
NEOMorpheus33 <jof6453@g.harvard.edu>
2021-02-24 20:27:24 -0500

    Update index.php

--- p1/VowelCounter/index.php
+++ p1/VowelCounter/index.php
@@ -1,32 +1,50 @@
-<!DOCTYPE html>   
-<head>
-    <title>e15p1 Palindrome? Project CSCI-E15 Harvard.edu HES</title> 
-    </head>
-    <meta>
-    </meta>
-<body> 
-<div class='container'> 
-<h2>String & Numbers Processor | Checks for Palindrome - e15 Project One</h2> 
-<form method="post">        
-Is it a Palindrome?: <input type="text" name="num"/><br>  
-<button type="submit">Process Info</button>   
-</form>  
-<?php   
-    if($_POST)  
-    {  
-        //get the value from form  
-        $num = $_POST['num'];  
-        //reversing the number  
-        $reverse = strrev($num);  
-          
-        //checking if the number and reverse is equal  
-        if($num == $reverse){  
-            echo "$num is a Palindrome";     
-        }else{  
-            echo "$num is not a Palindrome";     
-        }  
-}     
-      ?> 
-</div> 
+<html>
+<body>
+    <div>
+ <span style='color:green' <h1></h1>Vowel Counter E1P1 Joseph Fanning</hl> </span>
+ 
+ <form name="form" action="" method="get">
+  <input type="text" name="subject" id="subject" value="">
+</form>
+ 
+<?php 
+
+//Author Joseph Fanning Vowel Counter and Spaces 
+
+
+{
+
+ $string=($_GET['subject']);
+  
+   $vowels = array('a','e','i','o','u');
+    $len = strlen($string);
+    $num = 0;
+        
+    for($i=0; $i<$len; $i++){
+    if(in_array($string[$i], $vowels))
+            {
+                $num++;
+            }
+ echo "$num";
+        }
+ function countDigits( $string ) 
+{
+    return preg_match_all( "/[0-9]/", $string );
+ 
+}
+substr_count($string, ' ');
+ 
+        echo " Vowel Counter : <span style='color:orange; font-weight:bold;'>". $num."</span>";
+ echo "<br>";
+ 
+ 
+ echo "Space Counter :" . substr_count($string," ");
+ 
+    }
+    
+
+
+
+ ?>
 </body>
-</html> 
+</html>
