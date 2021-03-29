cab5ce8c7ffc7ea726da46b52ae06228fa2bed50
Joseph Fanning <jof6453@g.harvard.edu>
2021-03-29 15:36:11 -0400

    Set up views, routes, controllers for p2

---
+++ p2/resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>@yield('title', "Harvard Extension School Calculator | Joseph Fanning")</title>
+    <meta charset='utf-8'>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+        <link rel="stylesheet" href="styles/style.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+        
+        <style>
+
+        .center {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        </style>
+    @yield('head')
+</head>
+<body>
+<h1>Joe's Harvard | HES Grade Calculator</h1>
+
+@yield('content')
+</body>
+</html>
