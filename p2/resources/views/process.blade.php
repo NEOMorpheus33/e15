879109529ea3d4edb528c27e93240b5fc0f0006c
Joseph Fanning <jof6453@g.harvard.edu>
2021-03-31 13:27:46 -0400

    Testing p2 app on production

---
+++ p2/resources/views/process.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/main')
+
+@section('content')
+<p>Do I think I'm passing: {{ $passing }}</p>
+<p>I am {{ $school }}</p> 
+@endsection
--- p2/resources/views/welcome.blade.php
+++ p2/resources/views/welcome.blade.php
@@ -1,28 +1,27 @@
 
-@extends('layouts/main')
+@extends('layouts/main') 
 
 @section('content')
+
+
 <span style="color:green"><h1>Do I think I'm passing?</h1> </span> 
 
-<form action="/action_page3.php">
-  <input type="checkbox" id="yescheck" name="yescheck" value="Yes :-)"> 
-  <label for="yescheck"> Yes :-)</label><br>
-  <input type="checkbox" id="nocheck" name="nocheck" value="No :-(">
-  <label for="nocheck"> No :-(</label><br>
-  <input type="submit" value="Submit"> <br>
-  
-  </form> 
-  
+<form action="/process" method="GET">
+
+  <input type="checkbox"  name="passing" value="Yes"> 
+  <label for="yescheck"> Yes </label><br>
+  <input type="checkbox" name="passing" value="No">
+  <label for="nocheck"> No </label><br>
   <span style="color:green"><h2>I am?:</h2></span>
-  
-  <form action="/e15p2hesweb/action_pageradio.php"> 
-<input type="radio" name="gender" value="In pre-school">In pre-school  
-<input type="radio" name="gender" value="In grade school">In grade school 
-<input type="radio" name="gender" value="In high school">In high school  
-<input type="radio" name="gender" value="In an undergrad college">In an undergrad college 
-<input type="radio" name="gender" value="In an undergrad university">In an undergrad university 
-<input type="radio" name="gender" value="A graduate student in college">A graduate student in college
-<input type="radio" name="gender" value="A graduate student at a university">A graduate student at a university <br>   
+
+
+  <input type="radio" name="school" value="In pre-school">In pre-school  
+  <input type="radio" name="school" value="In grade school">In grade school 
+  <input type="radio" name="school" value="In high school">In high school  
+  <input type="radio" name="school" value="In an undergrad college">In an undergrad college 
+  <input type="radio" name="school" value="In an undergrad university">In an undergrad university 
+  <input type="radio" name="school" value="A graduate student in college">A graduate student in college
+  <input type="radio" name="school" value="A graduate student at a university">A graduate student at a university <br>   
 
 <input type="submit" value="Answer"> 
 
