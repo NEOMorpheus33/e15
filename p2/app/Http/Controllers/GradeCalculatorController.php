c6cea0628dda8e30d05c56160c37c0bea1245c11
Joseph Fanning <jof6453@g.harvard.edu>
2021-04-02 13:57:26 -0400

    filesp2

--- p2/app/Http/Controllers/GradeCalculatorController.php
+++ p2/app/Http/Controllers/GradeCalculatorController.php
@@ -5,29 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request; 
 
 class GradeCalculatorController extends Controller  
-    {
+{
     public function index() { 
         return view('welcome');  
         } 
 
-        
-
-    public function process(Request $request) {  
+        public function process(Request $request) {  
         $passing = $request->passing;
         $school = $request->school; 
         
         $request->validate([
         'passing' => 'required',
         'school' => 'required' 
+    ]);
 
-        ]);
+    dump($request->all()); 
 
-        dump($request->all()); 
+    }   
 
-        }   
-
-    
-    } 
+} 
 
 
 
