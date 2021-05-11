7c183e83ff0c34af13e5b70c59b33f055fc3eb66
Joseph Fanning <jof6453@g.harvard.edu>
2021-05-11 16:00:39 -0400

    1

--- p3/resources/views/index.blade.php
+++ p3/resources/views/index.blade.php
@@ -1,7 +1,13 @@
-@extends('layouts/main')                     
-@section('Search')     
+@extends('layouts/main')
+@section('Search')
 
-<?php
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+
+    <?php
 
 /*Authors/Authoring */
 /*Joe PF Fanning JPFF */
@@ -11,62 +17,118 @@
 /* https://www.extension.harvard.edu.*/
 /*JSearch Logo*/
 
-?> 
+?>
 
-<a href='/'><img src='/images/JSLogo4.jpg' id='logo' alt='Joe Search Engine Logo v1'> </a> 
+    <a href='/'><img src='/images/JSLogo4.jpg' id='logo' alt='Joe Search Engine Logo v1'> </a>
 
-<nav>   
-    <ul>
-        <!-- nav links  --> 
+    <nav>
+        <ul>
+            <!-- nav links  -->
 
-        <li> 
-            @if(!Auth::user()) 
-                <a href='/login'>Login</a> 
-                 @else 
+            <li>
+                @if(!Auth::user())
+                <a href='/login'>Login</a>
+                @else
                 <form method='POST' id='logout' action='/logout'>
                     {{ csrf_field() }}
-                    <a href='#' onClick='document.getElementById("logout").submit();'>Logout</a> 
-                    | <a href='/CRUD/create'>Add Site</a> 
-                </form> 
-                 @endif
-           
-        </li> 
-    </ul>
-</nav>  
+                    <a href='#' onClick='document.getElementById("logout").submit();'>Logout</a>
+                    | <a href='/CRUD/create'>Add Site</a>
+                </form>
+                @endif
 
-@if(Auth::user()) 
-<h2>
-    Hello {{ Auth::user()->name }}!
-</h2>
-@endif
+            </li>
+        </ul>
+    </nav>
 
-@section('content')
+    @if(Auth::user())
+    <h2>
+        Hello {{ Auth::user()->name }}!
+    </h2>
+    @endif
 
-<h1><a href='/register'>register</a> </h1>
+    @section('content')
+
+    <h1><a href='/register'>register</a> </h1>
 
 
-<table> 
-  
- <form action="/process" method="GET">     
+    <table>
 
-  <tr><td><input type="text" name="JoeSearch" value=" "> </td></tr>    
-  <tr><td><label for="text">Type Keywords Search</label></td></tr>  
-  
-  <tr><td><input type="checkbox" name="PrivateSearch" value=" "> </td></tr> 
-  <tr><td><label for="PublicSearch">  Private Search </label></td> </tr>         
+        <form action="/process" method="GET">
 
-</table>  
+            <tr>
+                <td><input type="text" name="JoeSearch" value=" "> </td>
+            </tr>
+            <tr>
+                <td><label for="text">Type Keywords Search</label></td>
+            </tr>
 
-<h2>Search Engine Results</h2> 
+            <tr>
+                <td><input type="checkbox" name="PrivateSearch" value=" "> </td>
+            </tr>
+            <tr>
+                <td><label for="PublicSearch"> Private Search </label></td>
+            </tr>
 
-<?php 
+    </table>
+
+    <h2>Search Engine Results</h2>
+
+    <?php 
 // Declaring variables
 $serps = "";
-{
-{$serps->url
-{$serps->title}
-{$serps->description}
-{$serps->body}
+
+
+<?php
+// define variables and set to empty values
+$title = $url = $body = $description $ITPro = ""; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $url = test_input($_POST["url"]);
+  $title = test_input($_POST["title"]);
+  $body = test_input($_POST["body"]);
+  $description = test_input($_POST["description"]);
+  
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  $data = save($serps);
+  return $data; 
 }
-}
+
 ?>
+
+    <h2>PHP Form Validation Example</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Name: <input type="text" name="url">
+        <br><br>
+        title: <input type="text" name="title">
+        <br><br>
+        descripton: <input type="text" name="description">
+        <br><br>
+        body: <textarea name="body" rows="5" cols="40"></textarea>
+        <br><br>
+        ITPro:
+        <input type="radio" name="ITPro" value="female">IT Professional
+        <input type="radio" name="NonITPro" value="male">Non IT Professional
+        <input type="radio" name="other" value="other">Other
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+echo "<h2>Your Input:</h2>";
+echo $url;
+echo "<br>";
+echo $title;
+echo "<br>";
+echo $description;
+echo "<br>";
+echo $body; 
+echo "<br>";
+echo $ITPro; 
+?>
+
+</body>
+</html>
