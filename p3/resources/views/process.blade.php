bba514a3bd31bdae9c04f0c737390bb537e4ddb2
Joseph Fanning <jof6453@g.harvard.edu>
2021-05-11 18:46:19 -0400

    m

--- p3/resources/views/process.blade.php
+++ p3/resources/views/process.blade.php
@@ -1,13 +1,12 @@
-@extends('layouts/main')    
-@section('content')  
-{{$request}} 
-{{$JoeSearch}}    
-{{$body}}  
-{{$processite}}  
-{{$sites}} 
-{{$PrivateSearch}} 
-{{$list}}   
+@extends('layouts/main')
+@section('content')
+{{$request}}
+{{$JoeSearch}}
+{{$body}}
+{{$processite}}
+{{$sites}}
+{{$PrivateSearch}}
+{{$list}}
 {{$serps}}
-{{searchTerms}}  
- @endsection 
-
+{{searchTerms}}
+@endsection
