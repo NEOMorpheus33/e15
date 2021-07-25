914e80dfdc7ea420a8796ae615ea2b2f1ab8d912
Joseph Fanning <Jof6453@g.harvard.edu>
2021-07-24 22:09:01 -0400

    track *.psd files using Git LFS

--- p3/resources/views/includes/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<p>
-    <span style='font-size:30px;'>&#9752; </span> Copyright 2021 |
-    <a href="https://www.joepfanning.com">Joseph P. Fitzgerald Fanning</a> | JPFF | Dr. Buck |
-    <a href="https://www.harvard.edu"> Harvard University App Engineers HES</a>
-    <span style='font-size:30px;'>&#9752;</span>
-</p>
